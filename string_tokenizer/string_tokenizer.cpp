9b328287e339f1b9995596035e76815462ca7e6c
algoshida <algoship@gmail.com>
2015-09-14 19:56:31 +0900

    int -> size_t

--- string_tokenizer/string_tokenizer.cpp
+++ string_tokenizer/string_tokenizer.cpp
@@ -5,11 +5,13 @@ using namespace std;
 vector<string> tokenizer(const string& s, const string& delim=" \n")
 {
 	vector<string> ret;
-	int prv = s.find_first_not_of(delim, 0);
+	size_t prv = s.find_first_not_of(delim, 0);
 	while(prv != string::npos){
-		int nxt = s.find_first_of(delim, prv);
+		size_t nxt = s.find_first_of(delim, prv);
 		ret.push_back(s.substr(prv, nxt - prv));
 		prv = s.find_first_not_of(delim, nxt);
 	}
 	return ret;
 }
+
+int main(){}
