2b8bd17f1935d0ab0ce3618b92e5c4544d3bfbc8
Hyun-Seok Oh <algoshipda@gmail.com>
2016-08-19 14:23:19 +0900

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define mp(a, b) make_pair((a), (b))
+#define pb(a) push_back((a))
+
+using namespace std;
+ 
+typedef long long lld;
+typedef pair<int, int> pii;
+typedef pair<lld, lld> pll;
+typedef pair<lld, int> pli;
+typedef pair<int, lld> pil;
+typedef vector<vector<int>> Matrix;
+typedef vector<vector<int>> Adj;
+typedef vector<int> Row;
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const lld LINF = 1e18;
+
+
+int main() {
+
+}
