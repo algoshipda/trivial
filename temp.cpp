e87c42ea970f87af4e1769facc0a656a4ee23447
Hyun-Seok Oh <algoshipda@gmail.com>
2016-10-04 22:22:44 +0900

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -12,7 +12,9 @@ typedef pair<int, lld> pil;
 typedef vector<vector<int>> Matrix;
 typedef vector<vector<int>> Adj;
 typedef vector<int> Row;
- 
+typedef complex<long double> Complex;
+typedef vector<Complex> Vcomplex;
+
 const int MOD = 1e9 + 7;
 const int INF = 1e9;
 const lld LINF = 1e18;
