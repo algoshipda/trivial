c167ca67b6c0c61272e6ef842db4d5630f8fa854
Hyun-Seok Oh <algoshipda@gmail.com>
2016-10-09 13:33:53 +0900

    Create temp2.cpp

---
+++ temp2.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <complex>
+
+#define mp(a, b) make_pair((a), (b))
+#define pb(a) push_back((a))
+#define pf(a) push_font((a))
+#define rb() pop_back()
+#define rf() pop_front()
+#define sz(a) ((int)a.size())
+
+using namespace std;
+ 
+typedef long long lld;
+typedef pair<int, int> pii;
+typedef pair<lld, lld> pll;
+typedef pair<lld, int> pli;
+typedef pair<int, lld> pil;
+typedef vector<vector<int>> Matrix;
+typedef vector<vector<int>> Adj;
+typedef vector<int> Row;
+typedef complex<double> Complex;
+typedef vector<Complex> Vcomplex;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const lld LINF = 1e18;
+const double FINF = 1e15;
+const double EPS = 1e-9;
+const double PI = 2.0 * acos(0.0);
+
+
+
+int main() {
+
+}
